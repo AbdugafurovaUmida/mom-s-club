024b0223fe8ac6b7565f09856e702939b485e905
Dildora Muslimova <faithpacifica@yahoo.com>
2024-01-30 21:56:04 +0900

    create footer section

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Instagram from '../../../public/images/instagram-icon.svg'
+import Telegram from '../../../public/images/telegram-icon.svg'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-[#504F4F] font-light py-[88px] text-white leading-6' >
+      <div className="container px-8">
+        <div className='flex flex-col justify-between md:items-end md:flex-row'>
+          <ul className='flex flex-col gap-3'>
+            <li className='mb-2'>Елизавета Евгеньевна Шац</li>
+            <li className='mb-2'>
+              <a href="tel:++7 999 999 99 99">+7 999 999 99 99</a>
+            </li>
+            <li className="flex gap-4 mb-2 socials">
+              <Link href="http://"><Image src={Instagram} alt="instagram logo" /></Link>
+              <Link href="http://"><Image src={Telegram} alt="tetegram logo" /></Link>
+            </li>
+            <li className='mb-2'>
+              <a className='underline' href="http://">Связаться</a>
+            </li>
+          </ul>
+          
+          <div className='flex order-3 mt-6 md:order-none opacity-60'> Разработка сайта GUD team </div>
+          
+          <div className='flex flex-col gap-3 underline md:items-end'>
+            <button className="" type="button">Наверх</button>
+            <a className="" href="http://">Политика конфиденциальности</a>
+            <a className="" href="http://">Договор-оферта</a>
+            <a className="" href="http://">Условия доставки</a>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
