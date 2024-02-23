40607cc6c46f1e4319b71bd99575a4f7f2024c9d
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-02-23 15:42:05 +0500

    feat: download attribute for buttons

--- src/components/common/BorderedButton.jsx
+++ src/components/common/BorderedButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const BorderedButton = ({ customClass = 'max-w-[250px]', children, customLink }) => {
+const BorderedButton = ({ customClass = 'max-w-[250px]', children, customLink, fileName }) => {
     return (
         <a className={`inline-block py-[18px] px-9 w-full max-w-[245px] ${customClass} bg-transparent border-[3px] border-solid border-secondary rounded-[30px] text-base font-regular text-center`}
-        href={customLink}>
+        href={customLink}
+        download={fileName ? fileName : ''}>
            {children}
         </a>
     )
