ce074e67ea60c7bcaeb5d93be51394a7c7fea963
Dildora Muslimova <faithpacifica@yahoo.com>
2024-02-27 23:09:13 +0900

    footer button style fix

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import style from './Footer.css';
 import React from 'react'
 import Instagram from '../../../public/images/instagram-icon.svg'
 import Telegram from '../../../public/images/telegram-icon.svg'
@@ -32,15 +32,28 @@ const Footer = () => {
               href='https://www.instagram.com/gud_team_/'
               className='font-bold transition duration-300 opacity-80 font-white hover:opacity-100 hover:underline hover:scale-125 hover:translate-x-6'> GUD team </a> </p>
 
-          <div className='flex flex-col md:items-end'>
-            <button
+          <div className='flex flex-col md:items-end '>
+            <div className="whatsapp">
+              <button onClick={scrollToTop} className="whatsapp__wrapper relative lg:static top-[32px]  flex mb-2 justify-center items-center  " >
+                <span className="whatsapp__waves">
+                  <span className="whatsapp__wave whatsapp__wave_1"></span>
+                  <span className="whatsapp__wave whatsapp__wave_2"></span>
+                  <span className="whatsapp__wave whatsapp__wave_3"></span>
+                </span>
+                <svg width={40} className="whatsapp__icon icon__arrow-up fill-white" viewBox="0 0 24 24">
+                  <title className='rounded-full'>Back to top</title>
+                  <path d="M18.71,11.71a1,1,0,0,1-1.42,0L13,7.41V19a1,1,0,0,1-2,0V7.41l-4.29,4.3a1,1,0,0,1-1.42-1.42l6-6a1,1,0,0,1,1.42,0l6,6A1,1,0,0,1,18.71,11.71Z" />
+                </svg>
+              </button>
+            </div>
+            {/* <button
               onClick={scrollToTop}
               className="absolute lg:static top-[32px] right-[32px] transition duration-300 flex mb-2 w-[56px] h-[56px] rounded-full justify-center items-center border border-pink border-2 bg-[#dfc5c4] hover:bg-transparent hover:translate-y-[7px] shadow-buttonShadow" type="button">
               <svg width={30} className="icon__arrow-up fill-white" viewBox="0 0 24 24">
                 <title className='rounded-full'>Back to top</title>
                 <path d="M18.71,11.71a1,1,0,0,1-1.42,0L13,7.41V19a1,1,0,0,1-2,0V7.41l-4.29,4.3a1,1,0,0,1-1.42-1.42l6-6a1,1,0,0,1,1.42,0l6,6A1,1,0,0,1,18.71,11.71Z" />
               </svg>
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
