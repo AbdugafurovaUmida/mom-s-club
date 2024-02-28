5e7c5eaa5d6077637abbe9b6e3abaaefc5cb3ef9
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-02-29 02:22:03 +0500

    fix: modal form

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 
 // const Modal = ({modalbot}) => {
@@ -21,41 +21,84 @@ import axios from 'axios';
 
 
 
-  export default function Modal() {
-    // const BOT_API_TOKEN = 'TOKEN'; // Replace 'TOKEN' with your actual bot token
-   const [message, setMessage] = useState('')
+export default function Modal() {
+  // const BOT_API_TOKEN = 'TOKEN'; // Replace 'TOKEN' with your actual bot token
 
-  const sendMessageToTelegram = async (message) => {
-   
+
+  const [name, setName] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  const sendMessageToTelegram = async (name, lastName, phoneNumber) => {
+
     try {
       const response = await axios.post(`https://api.telegram.org/bot7026888986:AAHZ7fHmJqRTZIY1bAriWY2jsj7BElobKsk/sendMessage`, {
         chat_id: '484404492', // Replace 'CHAT_ID' with your actual chat ID
-        text: message,
+        text: {
+          'Ismi': name,
+          'Familyasi': lastName,
+          'Telefon raqami': phoneNumber 
+        },
       });
+      // {   }
       console.log('Message sent:', response.data);
     } catch (error) {
-       console.error('Error sending message:', error);
-     }
-   };
-   const handleSend = () => {
-    sendMessageToTelegram(message)
+      console.error('Error sending message:', error);
+    }
+  };
 
-   
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name && lastName && phoneNumber) {
+      if(phoneNumber !== ''){
+        sendMessageToTelegram(name, lastName, phoneNumber)
+        setName('')
+        setPhoneNumber('')
+        setLastName('')
+      }else{
+        console.log('please enter a phonenumber')
+      }
+    }
   }
 
   return (
 
     <>
-          <h3>Welcome to My Telegram App</h3>
-           <input
-           type='text'
-             placeholder="Type your message..."
-             onChange={e => setMessage(e.target.value)}
-           />
-           <button onClick={handleSend()} title="Send" >send</button>
-        </>
-     
-       );
-     }
-    
- 
+      <form action="" onSubmit={handleSubmit}>
+        <h3>Welcome to My Telegram App</h3>
+        <div>
+          <input
+            type='text'
+            placeholder="Type your Name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            placeholder='Type your phone Last Name'
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input t
+            type="text"
+            placeholder='Type your phone Number' 
+            value={phoneNumber}
+            onChange={e => setPhoneNumber(e.target.value)}
+            />
+        </div>
+
+
+        <button type='submit' title="Send" >send</button>
+      </form>
+    </>
+
+  );
+}
+
