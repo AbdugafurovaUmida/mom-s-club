00998604842f85ea0a7658a08f2e69edf55c09c8
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2024-09-27 11:47:04 +0500

    fatch:create seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl:
+    'https://www.momsclub.uz/', 
+    generateRobotsTxt: true,
+}
--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,21 @@ import Products from '@/components/Products'
 import React from 'react'
 import Sponsors from '@/components/Sponsors'
 import Gallery from '@/components/Gallery'
+import Head from 'next/head'
 
 
 const HomePage = () => {
   return (
+    <>
+    <Head>
+      <title> Mom's club</title>
+      <meta name= "description" content="Mom's club - Первый курс по уходу за новорождёнными для мам в Узбекистане"/>
+      <meta property='og:title' content='Mom`s club'/>
+      <meta property='og:description' content='Первый курс по уходу за новорождёнными для мам в Узбекистане'/>
+      <meta property='og:type' content='website'/>
+      <meta property='og:image' content='/images/logo.png'/>
+      <meta name="robots" content="index, follow"/>
+      </Head>
     <main className='h-full min-h-screen'>
       <Hero />
       <div className='course-author__wrapper'>
@@ -22,6 +33,8 @@ const HomePage = () => {
       <Materials />
      
     </main>
+    
+    </>
   )
 }
 
